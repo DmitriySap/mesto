efc2c9a03af92e501bebdd1d659f3ace63ed2307
Dmitriy Saprykin <saprykindima@bk.ru>
2022-06-17 01:59:16 +0300

    begin

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,37 @@ const profileDescription = document.querySelector('.profile__description');
 const descriptionField = document.querySelector('.popup__input_content_description');
 const formEl = document.querySelector('.popup__form');
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+function createCard(name, link) {
+  
+}
+
 function openPopup(popupEl) {
     popupEl.classList.add('popup_type_is-open');
 }
